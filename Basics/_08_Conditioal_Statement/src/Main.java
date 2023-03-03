4ee7c52ccf52cb2f0487c4c34907d6530eb8a959
talha4t <mahmudtalha80@gmail.com>
2023-03-03 11:13:21 +0600

    Condition

--- Basics/_08_Conditioal_Statement/src/Main.java
+++ Basics/_08_Conditioal_Statement/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean isSunUp = true;
+
+        if (isSunUp)
+            System.out.println("Day");
+        else
+            System.out.println("Night");
+
+        int age = 19;
+        if (age >= 18)
+            System.out.println("Can Vote");
+        else
+            System.out.println("Can't Vote");
     }
 }
