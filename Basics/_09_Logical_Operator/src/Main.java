9973363f69b30339106854e9e461fcd074383a47
talha4t <mahmudtalha80@gmail.com>
2023-03-03 11:20:55 +0600

    Logical operator

--- Basics/_09_Logical_Operator/src/Main.java
+++ Basics/_09_Logical_Operator/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 30;
+        int b = 40;
+        // && (AND)
+        if (a < 50 && b < 50)
+            System.out.println("Both less than 50");
+
+        int c = 100, d = 10;
+        // || (OR)
+        if (c < 50 || d < 50)
+            System.out.println("At least one less than 50");
+
+        boolean x = false;
+        // ! (NOT)
+        if (!x)
+            System.out.println("Its not false");
+
     }
 }
