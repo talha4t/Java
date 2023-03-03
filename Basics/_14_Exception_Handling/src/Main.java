cd4dd3941009c6785379853e19dad7132831adf2
talha4t <mahmudtalha80@gmail.com>
2023-03-03 11:47:31 +0600

    Exception Handling

---
+++ Basics/_14_Exception_Handling/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] marks = {98, 99, 90};
+
+        try {
+            System.out.println(marks[5]);
+        }  catch(Exception exception) {
+            // do something after catching
+        }
+
+        System.out.println("Talha");
+
+    }
+}
