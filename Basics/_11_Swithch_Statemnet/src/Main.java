612e8f553fa0bf55390620f3863c730d623f7970
talha4t <mahmudtalha80@gmail.com>
2023-03-03 11:29:46 +0600

    Switch Statement

--- Basics/_11_Swithch_Statemnet/src/Main.java
+++ Basics/_11_Swithch_Statemnet/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int day = 1;
+
+        switch(day) {
+            case 1 :
+                System.out.println("Monday");
+                break;
+            case 2 :
+                System.out.println("Tuesday");
+                break;
+            default :
+                System.out.println("Wednesday -> Sunday");
+        }
     }
 }
