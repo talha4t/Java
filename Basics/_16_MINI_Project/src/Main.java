1c09431304a62075287b15722cb8d13d6aae7fab
talha4t <mahmudtalha80@gmail.com>
2023-03-03 12:11:10 +0600

    Mini Porject

---
+++ Basics/_16_MINI_Project/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double n = ((double)Math.random() * 100);
+        int number = (int)n;
+
+        int userNumber = 0, guessSteps = 0;
+        do {
+            System.out.println("Guess my Number : ");
+            userNumber = sc.nextInt();
+
+            guessSteps++;
+            if (userNumber == number) {
+                System.out.println("Yay!!!....You guess the correct One");
+                break;
+            }
+            else if (userNumber > number)
+                System.out.println("Your number is too large");
+            else
+                System.out.println("Your number is too small");
+        } while(userNumber >= 0);
+
+        System.out.print("The Number was : " + number);
+        if (guessSteps > 1)
+            System.out.print(" And you Took : " + guessSteps + " move to find it.");
+        else
+            System.out.println(" And you Took : " + guessSteps + " moves to find it.");
+    }
+}
