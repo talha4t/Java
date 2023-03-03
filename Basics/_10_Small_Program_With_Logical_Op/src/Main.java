a8b4828adba52f713dfa356c1b5eb399d78e938d
talha4t <mahmudtalha80@gmail.com>
2023-03-03 11:26:02 +0600

    Smalle code

--- Basics/_10_Small_Program_With_Logical_Op/src/Main.java
+++ Basics/_10_Small_Program_With_Logical_Op/src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        // pen = 10, notebook = 40
+
+        int cash = sc.nextInt();
+
+        if (cash < 10) {
+            System.out.println("Can't buy anything");
+            System.out.println("Get more cash");
+        }
+        else if (cash > 10 && cash < 50)
+            System.out.println("Can get one thing");
+        else
+            System.out.println("Can get both");
+
+
+
     }
 }
