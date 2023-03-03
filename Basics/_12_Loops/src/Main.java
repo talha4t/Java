d58c9ee2faf28785ef78ff50798db5848cc74b51
talha4t <mahmudtalha80@gmail.com>
2023-03-03 11:36:48 +0600

    12 Loops

--- Basics/_12_Loops/src/Main.java
+++ Basics/_12_Loops/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // For loop
+        for (int i = 1; i <= 100; i++)
+            System.out.println(i);
+
+        // 100 to 1
+        for (int i = 100; i >= 1; i--)
+            System.out.print(i + " ");
+
+        System.out.println("");
+        //While loop
+        int i = 100;
+        while(i >= 1) {
+            System.out.print(i + " ");
+            i--;
+        }
+
+        System.out.println("");
+        // Do while
+        int j = 1;
+        do {
+            System.out.print(j + " ");
+            j++;
+        } while(j <= 100);
     }
 }
