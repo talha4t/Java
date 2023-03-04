bd1b283427893c3ac5324a910191515e376a63f2
talha4t <mahmudtalha80@gmail.com>
2023-03-04 11:26:38 +0600

    Constructor

--- OOP/_02_Constructor/src/Main.java
+++ OOP/_02_Constructor/src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Student s1 = new Student();
+
+        s1.greetings();
+        
+        System.out.println(s1.rno);
+        System.out.println(s1.name);
+        System.out.println(s1.marks);
+
     }
+}
+
+class Student {
+    int rno;
+    String name;
+    float marks;
+
+    void greetings() {
+        System.out.println("Hello! My name is " + name);
+    }
+    // Basically it replace with all objects name which is named with Student() Class
+
+    // this. use for Constructor
+    Student () {
+        this.rno = 550785;
+        this.name = "Talha Mahmud";
+        this.marks = 99.0f;
+    }
+
 }
